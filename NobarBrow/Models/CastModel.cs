837e1b73551cffd4d133ee6521f60a4e48eb2046
juanprasetyo <juanprasetyo040502@gmail.com>
2023-01-08 22:39:20 +0700

    buat model untuk aktor/aktress

---
+++ NobarBrow/Models/CastModel.cs
@@ -0,0 +1,11 @@
+namespace NobarBrow.Models
+{
+    public class CastModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Gender { get; set; }
+        public string ProfilePath { get; set; }
+        public string Character { get; set; }
+    }
+}
